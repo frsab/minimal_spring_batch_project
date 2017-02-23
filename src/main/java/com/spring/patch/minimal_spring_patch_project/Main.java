ea4c6d35918eecd2ff26b5525a7e808d30a4ec9b
Saber <frejsaber@gmail.com>
2017-02-23 09:38:14 +0100

    mise à niveau de dependencies et les versions (pom.xml)

--- src/main/java/com/spring/patch/minimal_spring_patch_project/Main.java
+++ src/main/java/com/spring/patch/minimal_spring_patch_project/Main.java
@@ -5,8 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class Main {
 
 	public static void main(String[] args) {
-	    System.exit(SpringApplication.exit(SpringApplication.run(
-		        BatchConfiguration.class, args)));
+	    System.exit(SpringApplication.exit(SpringApplication.run(BatchConfiguration.class, args)));
 
 	}
 }
