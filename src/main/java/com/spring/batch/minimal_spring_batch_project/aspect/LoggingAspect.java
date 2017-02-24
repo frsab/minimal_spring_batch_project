de2090d9bec262a7f6ecf659715e5fccfc539f66
Saber <frejsaber@gmail.com>
2017-02-24 15:41:20 +0100

    logging Aspect

---
+++ src/main/java/com/spring/batch/minimal_spring_batch_project/aspect/LoggingAspect.java
@@ -0,0 +1,19 @@
+package com.spring.batch.minimal_spring_batch_project.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+	private static Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+	 @Before("execution(* com.spring.batch.minimal_spring_batch_project.bean.SameService.showString(java.lang.String))")// && args(sampleName)")
+	    public void beforeShowString(String sampleName) {
+		 logger.info("A request was issued for a sample name: "+sampleName);
+	    }
+	
+
+}
